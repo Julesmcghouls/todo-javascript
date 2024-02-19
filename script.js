705f24825831c4325ace4bdc603c229d8d10c775
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2024-02-18 22:24:16 -0600

    Add numbers to array and calculate sum

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ if (add.length > 0) {
 sum += add.pop();
   console.log(sum); // 19
 }
+
+add.push(10, 20, 30, 40, 50);
+console.log(add); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50]
+
+let sum2 = add.pop();
+console.log(sum2); // 50
+
+
+// Path: script.js
+
